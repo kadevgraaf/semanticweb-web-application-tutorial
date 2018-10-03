7a43231a61e21bc6ffdfd0d5b88ce55665895c7d
kmitd <ilaria.tiddi@open.ac.uk>
2018-10-03 17:55:12 +0200

    added index.html + main.js files for class

---
+++ Assignment5/main.js
@@ -0,0 +1,22 @@
+angular.module('KRRclass', [ 'chart.js']).controller('MainCtrl', ['$scope','$http', mainCtrl]);
+
+
+function mainCtrl($scope, $http){
+  	
+	// TODO : type here code for your Ex 1
+	
+	
+	
+	// TODO : type here code for your Ex 2 
+
+
+
+	// TODO : type here code for your Ex 2 
+	$scope.doMyAction = function(){
+		
+		
+	};
+	
+}
+
+
